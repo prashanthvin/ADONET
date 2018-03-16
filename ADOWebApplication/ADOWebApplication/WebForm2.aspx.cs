943aa4098a227e7cd539766e12de37eed6048d28
Prashanth V <prashanthvsub@gmail.com>
2018-03-15 22:35:48 -0400

    Initial commit

---
+++ ADOWebApplication/ADOWebApplication/WebForm2.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ADOWebApplication
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from tblStudents", con);
+                con.Open();
+
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+    }
+}
